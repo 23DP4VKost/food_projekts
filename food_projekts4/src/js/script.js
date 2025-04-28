27e272c81f66b96cca101d847e6a8bfc11db633c
Valērijs Kostjukevičs <166384039+23DP4VKost@users.noreply.github.com>
2025-04-28 09:30:28 +0000

    smooth navigation -> script.js

--- food_projekts4/src/js/script.js
+++ food_projekts4/src/js/script.js
@@ -1,3 +1,19 @@
+
+document.querySelectorAll(".nav-bar a").forEach((link) => {
+    link.addEventListener("click", function (event) {
+        event.preventDefault();
+        const targetId = this.getAttribute("href").substring(1); 
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: "smooth", 
+                block: "start",
+            });
+        }
+    });
+});
+
 document.getElementById("download-button").addEventListener("click", function() {
     const link = document.createElement("a");
     
